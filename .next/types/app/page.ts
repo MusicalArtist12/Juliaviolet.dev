72b000d0f935c18007b7e788ca05ed3b1377eaba
Julia <Julia.Violet@outlook.com>
2023-06-22 14:22:27 -0700

    commit

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,8 +1,8 @@
 // File: /home/julia/Documents/projects/MusicalArtist12.github.io/app/page.tsx
-import * as entry from '../../../app/page.js'
+import * as entry from '../../../app/page.jsx'
 import type { ResolvingMetadata } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../app/page.js')
+type TEntry = typeof import('../../../app/page.jsx')
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode
-}) {
+import '../styles/main.css'
+
+export default function RootLayout( { children } : { children: React.ReactNode }) {
+    // root layout, must have html and body tag
     return (
         <html lang="en">
             <body>{children}</body>
