d7e2ebe02a1e42ef35764b3439ef4c95821f5a98
MusicalArtist12 <Themusicalartist12@gmail.com>
2023-08-13 17:13:57 -0400

    people?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 // juliaviolet.dev/
-import Head from 'next/head';
+import Link from 'next/link';
 
 import { Metadata } from 'next'
  
@@ -13,7 +13,8 @@ export default function Page() {
                 <h2>This page is not finished</h2>
                 <p>I guess i should also greet react-spring...</p>
                 <p>You have reached my new webpage! More is to come in the future, hold on tight.</p>
-                <p>June 18th Update: <a href="/About-Me">/About Me</a> has content on it!</p>
+                <p>June 18th Update: <Link href="/About-Me">/About Me</Link> has content on it!</p>
+                <p>August 13th update: We got a loading screen, <Link href="/Projects">/Projects</Link>, <code> &#123;harder, better, faster, stronger&#125;</code> CSS, oh and a <Link href="/Resume.pdf">resume</Link> if you care.</p>
             </div>
         </>
     )
