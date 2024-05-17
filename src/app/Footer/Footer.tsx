ae4525bcc9b44bcda0cc55d281a62d0448bc246e
Julia <44149950+MusicalArtist12@users.noreply.github.com>
2024-05-17 10:34:09 -0700

    Thats embarassing

--- src/app/Footer/Footer.tsx
+++ src/app/Footer/Footer.tsx
@@ -36,7 +36,7 @@ export default async function Footer() {
             </div>
             <Oneko startX={16} startY={16} startAlert={false} nekoSites={friendSites}/>
             <header>
-                <p><Link href="https://github.com/MusicLinklArtist12/Juliaviolet.dev">{GetLogo("Github", "1x")} MusicalArtist12/Juliaviolet.dev</Link> | 2023</p> 
+                <p><Link href="https://github.com/MusicalArtist12/Juliaviolet.dev">{GetLogo("Github", "1x")} MusicalArtist12/Juliaviolet.dev</Link> | 2023</p> 
                 <p>Made with Next.js and :3 </p>
             </header>
  
