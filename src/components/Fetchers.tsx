84be7c240b1cc2bb1346c8b7faa0ef1ea4c738e0
Julia <themusicalartist12@gmail.com>
2024-10-08 22:08:35 -0700

    add path.join() and maybe this will work?

--- src/components/Fetchers.tsx
+++ src/components/Fetchers.tsx
@@ -1,19 +1,20 @@
-import { promises as fs } from 'fs';
+import { promises as fs } from 'fs'
+import path from 'path'
 
 export async function getDirectory() {
-    const DB = await fs.readFile(process.cwd() + "/public/Data/Directory.json", 'utf-8')
+    const DB = await fs.readFile(path.join(process.cwd() + "/public/Data/Directory.json"), 'utf-8')
 
     return JSON.parse(DB)
 }
 
 export async function getPhotos() {
-    const DB = await fs.readFile(process.cwd() + "/public/Data/juliaPhotos.json", 'utf-8')
+    const DB = await fs.readFile(path.join(process.cwd() + "/public/Data/juliaPhotos.json"), 'utf-8')
 
     return JSON.parse(DB).Photos
 }
 
 export async function getContacts() {
-    const DB = await fs.readFile(process.cwd() + "/public/Data/contactInfo.json", 'utf-8')
+    const DB = await fs.readFile(path.join(process.cwd() + "/public/Data/contactInfo.json"), 'utf-8')
 
     return JSON.parse(DB)
 }
